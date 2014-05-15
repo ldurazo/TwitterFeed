70c6f9586f65042640b732dd1224896e3449ddb4
ldurazo <ldurazo@nearsoft.com>
2014-05-15 15:31:45 -0700

    Second commit

--- app/src/main/java/com/internship/twitterfeed/app/MainActivity.java
+++ app/src/main/java/com/internship/twitterfeed/app/MainActivity.java
@@ -1,18 +1,35 @@
 package com.internship.twitterfeed.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Tweet tweet = new Tweet();
+        tweet.author = "ringo";
+        tweet.content = "test1 as long as possible cuz f you thats why";
+        ArrayList<Tweet> items = new ArrayList<Tweet>();
+        items.add(tweet);
+
+        TweetListAdaptor adaptor = new TweetListAdaptor(this,R.layout.list_item, items);
+        setListAdapter(adaptor);
     }
+
 
 
     @Override
@@ -20,6 +37,7 @@ public class MainActivity extends ActionBarActivity {
         
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
@@ -35,4 +53,33 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private class TweetListAdaptor extends ArrayAdapter<Tweet> {
+
+        private ArrayList<Tweet> tweets;
+
+        public TweetListAdaptor(Context context, int textViewResourceId,  ArrayList<Tweet> items) {
+            super(context, textViewResourceId, items);
+            this.tweets = items;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater)
+                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.list_item, null);
+            }
+            Tweet o = tweets.get(position);
+
+            TextView tt = (TextView) v.findViewById(R.id.toptext);
+            TextView bt = (TextView) v.findViewById(R.id.bottomtext);
+            tt.setText(o.author);
+            bt.setText(o.content);
+
+            return v;
+        }
+    }
 }
+
+
---
+++ app/src/main/java/com/internship/twitterfeed/app/Tweet.java
@@ -0,0 +1,11 @@
+package com.internship.twitterfeed.app;
+
+/**
+ * Created by ldurazo on 5/15/2014.
+ */
+public class Tweet {
+
+    String author;
+    String content;
+
+}
