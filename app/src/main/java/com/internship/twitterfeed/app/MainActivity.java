a0f35ab811448c4e33f386176331270f0f9db787
ldurazo <ldurazo@nearsoft.com>
2014-05-16 17:42:42 -0700

    Third commit

--- app/src/main/java/com/internship/twitterfeed/app/MainActivity.java
+++ app/src/main/java/com/internship/twitterfeed/app/MainActivity.java
@@ -1,62 +1,94 @@
 package com.internship.twitterfeed.app;
 
+
+import java.util.ArrayList;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import java.util.ArrayList;
-
 
 public class MainActivity extends ListActivity {
+    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Tweet tweet = new Tweet();
-        tweet.author = "ringo";
-        tweet.content = "test1 as long as possible cuz f you thats why";
-        ArrayList<Tweet> items = new ArrayList<Tweet>();
-        items.add(tweet);
+        new MyTask().execute();
 
-        TweetListAdaptor adaptor = new TweetListAdaptor(this, R.layout.list_item, items);
-        setListAdapter(adaptor);
     }
 
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+        private ProgressDialog progressDialog;
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "", "Loading. Please wait...", true);
         }
-        return super.onOptionsItemSelected(item);
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+
+                HttpClient hc = new DefaultHttpClient();
+                HttpGet get = new
+                        HttpGet("http://search.twitter.com/search.json?q=android");
+
+                HttpResponse rp = hc.execute(get);
+
+                if(rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
+                {
+                    String result = EntityUtils.toString(rp.getEntity());
+                    JSONObject root = new JSONObject(result);
+                    JSONArray sessions = root.getJSONArray("results");
+                    for (int i = 0; i < sessions.length(); i++) {
+                        JSONObject session = sessions.getJSONObject(i);
+
+                        Tweet tweet = new Tweet();
+                        tweet.content = session.getString("text");
+                        tweet.author = session.getString("from_user");
+                        tweets.add(tweet);
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("TwitterFeedActivity", "Error loading JSON", e);
+            }
+            return null;
+
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            progressDialog.dismiss();
+            setListAdapter(new TweetListAdaptor(
+                    MainActivity.this, R.layout.list_item, tweets));
+        }
+
     }
 
     private class TweetListAdaptor extends ArrayAdapter<Tweet> {
 
         private ArrayList<Tweet> tweets;
 
-        public TweetListAdaptor(Context context, int textViewResourceId,  ArrayList<Tweet> items) {
+        public TweetListAdaptor(Context context,
+
+                                int textViewResourceId,
+                                ArrayList<Tweet> items) {
             super(context, textViewResourceId, items);
             this.tweets = items;
         }
@@ -73,12 +105,10 @@ public class MainActivity extends ListActivity {
 
             TextView tt = (TextView) v.findViewById(R.id.toptext);
             TextView bt = (TextView) v.findViewById(R.id.bottomtext);
-            tt.setText(o.author);
-            bt.setText(o.content);
+            tt.setText(o.content);
+            bt.setText(o.author);
 
             return v;
         }
     }
 }
-
-
